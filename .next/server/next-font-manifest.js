917f59cbef1d7c2db6528a8af8cf222e0db86d74
Ramkesh <ramkeshse@gmail.com>
2025-05-31 21:07:40 +0000

    ds

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/var/www/html/Glamo-fashion-admin/app/layout":["static/media/e4af272ccee01ff0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
