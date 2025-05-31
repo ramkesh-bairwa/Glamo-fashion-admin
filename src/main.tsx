3622a544be7b31f5148146e7c0c79d69d0aa097b
Ramkesh Bairwa <ramkeshse@gmail.com>
2025-06-01 03:25:28 +0530

    added

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/admin">
         <App />
         <ToastContainer
           position="top-right"
