3e33ddbdc8dc277544050eb89cc955c2050e2e67
Ramkesh Bairwa <ramkeshse@gmail.com>
2025-05-31 14:23:51 +0530

    post changed with 5000

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 exec(`next dev -p ${port}`, (err, stdout, stderr) => {
   if (err) {
