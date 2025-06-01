dcd890fff911a5e331b6fbbf1821c849b0083b8c
Ramkesh Bairwa <ramkeshse@gmail.com>
2025-06-01 23:47:22 +0530

    added

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "glamo-admin",
-      script: "npx serve -s dist -l 5000",
-      interpreter: "node", // Important: This runs 'serve' as a Node.js CLI tool
+      script: "serve",
+      args: ["-s", "dist", "--listen=5000"],
       env: {
         NODE_ENV: "production",
       },
