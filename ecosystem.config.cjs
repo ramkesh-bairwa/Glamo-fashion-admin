07e158a19f95c01599bce8bfda26d5ab3715ddb0
Ramkesh Bairwa <ramkeshse@gmail.com>
2025-06-02 00:50:23 +0530

    deploy now

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,10 @@
-// ecosystem.config.cjs
 module.exports = {
   apps: [
     {
       name: "glamo-admin",
-      script: "npx",
-      args: ["serve", "-s", "dist", "--listen=3001"],
+      script: "node_modules/next/dist/bin/next",
+      args: ["start", "-p", "3001"],
       exec_mode: "fork",
-      wait_ready: false,
       env: {
         NODE_ENV: "production",
       },
