55d2ed3c4e38ec299e324e005acf38dfd9700600
Ramkesh Bairwa <ramkeshse@gmail.com>
2025-06-02 00:20:29 +0530

    ads

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,9 +1,12 @@
+// ecosystem.config.cjs
 module.exports = {
   apps: [
     {
       name: "glamo-admin",
       script: "npx",
       args: ["serve", "-s", "dist", "--listen=5000"],
+      exec_mode: "fork",
+      wait_ready: false,
       env: {
         NODE_ENV: "production",
       },
